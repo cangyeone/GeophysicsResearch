f37dbf4f2bec97e58465eefe41a09ca82fcdee93
cangyeone <305601470@qq.com>
2017-03-06 17:23:35 +0800

    add some new file
    no discription

---
+++ SeismicClassification/SC_refine.py
@@ -0,0 +1,248 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  2 10:07:05 2017
+
+@author: LLL
+"""
+
+try:
+    import tensorflow as tf
+except:
+    import os
+    os.system('pip install tensorflow')
+finally:
+    import xlrd
+    import tensorflow as tf
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    import time
+
+    
+class Classfication():
+    def ReadExcelFile(self,fileName,classIdx):
+        workBook=xlrd.open_workbook(fileName)
+        sheet=workBook.sheet_by_index(0)
+        nrows=sheet.nrows
+        val=[]
+        self.attri=sheet.row_values(0)
+
+        for it in range(1,nrows):
+            val.append(sheet.row_values(it))
+        def Replace(x):
+            if(x[classIdx]=='是' or x[classIdx]==1):
+                rt=[1.,0.]
+                sp=['+']
+            else:
+                rt=[0.,1.]
+                sp=['o']
+            return [x[:classIdx],rt,sp]
+        val=list(map(Replace,val))
+        dt=[]
+        cor=[]
+        clr=[]
+        for itr in val:
+            dt.append(itr[0])
+            cor.append(itr[1])
+            clr.append(itr[2])
+        return dt,cor,list(range(len(dt))),clr
+    def Norm(self,data):
+        self.NormCnt=0
+        if(self.NormCnt==0):
+            self.mini=np.min(data,axis=0)
+        sc=np.subtract(data,self.mini)
+        if(self.NormCnt==0):
+            self.maxa=np.max(sc,axis=0)
+        mx=np.multiply(np.divide(sc,self.maxa),0.8)
+        mx=np.add(mx,0.1)
+        self.NormCnt=self.NormCnt+1
+        return mx,self.maxa,self.mini
+        
+    def GenTrainData(self,data,vali,idx,num=10):
+        sel=np.random.choice(idx,num)
+        rdt=[]
+        rval=[]
+        for itr in sel:
+            rdt.append(data[itr])
+            rval.append(vali[itr])
+        return np.array(rdt),np.array(rval)
+    
+    def GenDataFromExcel(self,fileName=r'index.xlsx',num=10):
+        data,vali,idx=self.ReadExcelFile(fileName)
+        data=self.Norm(data)
+        rd,rv=self.GenTrainData(data,vali,idx,num)
+        return rd,rv
+    
+    def my_weigh(self,shape):
+        #init=tf.truncated_normal(shape,stddev=0.1)
+        init=tf.constant(0.1,shape=shape)
+        return tf.Variable(init)
+    def my_bias(self,shape):
+        init=tf.constant(0.1,shape=shape)
+        return tf.Variable(init) 
+    def my_actv(self,aa):
+        return tf.nn.sigmoid(aa)
+    def LayerToFcl(self,data):
+        re=[]
+        for itr in range(len(data)-1):
+            re.append([data[itr],data[itr+1]])
+        return re
+    def FitFunc(self,x,a,b,c,d,e,f):
+        re=a*np.exp(b*x*x+c*x+d)+e
+        return re
+    def __init__(self,fileName,valiName,layer=[2,2]):
+        
+        fcl=self.LayerToFcl(layer)
+        
+        x=tf.placeholder(tf.float32,[None,fcl[0][0]])
+        y=tf.placeholder(tf.float32,[None, 2])
+        kp1=tf.placeholder(tf.float32)
+        kp2=tf.placeholder(tf.float32)
+        kp3=tf.placeholder(tf.float32)
+        fc_w=[]
+        fc_b=[]
+        fc_fn=[]
+        fc=[]
+        for itr in range(len(fcl)):
+            fc_w.append(self.my_weigh(fcl[itr]))
+            fc_b.append(self.my_bias([fcl[itr][1]]))
+            if(itr==0):
+                fc_fn.append(tf.matmul(x,fc_w[itr])+fc_b[itr])
+            else:
+                fc_fn.append(tf.matmul(fc[itr-1],fc_w[itr])+fc_b[itr])
+            fc.append(self.my_actv(fc_fn[itr]))
+        nFcl=len(fcl)
+
+        ce=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=fc[nFcl-1]))
+        #ce=-tf.reduce_mean(fc2*y)
+        ts=tf.train.AdamOptimizer(1e-2).minimize(ce)
+        
+        correct_prediction = tf.equal(tf.argmax(fc[nFcl-1],1), tf.argmax(y,1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        
+        
+        sess=tf.Session()
+        sess.run(tf.global_variables_initializer())
+        
+        train_data_t,train_vali,train_idx,train_clr=self.ReadExcelFile(fileName,classIdx=fcl[0][0])
+        data_t,vali,idx,clr=self.ReadExcelFile(valiName,classIdx=fcl[0][0])
+        
+        tdata=np.transpose(data_t)
+        data,maxa,mini=self.Norm(data_t)
+        train_data,tmaxa,tmini=self.Norm(train_data_t)
+        st=time.clock()
+        for itr in range(3000):
+            rd,rv=self.GenTrainData(train_data,train_vali,train_idx,30)
+            sess.run(ts,feed_dict={x:rd,y:rv,kp1:1,kp2:1,kp3:1})
+            if(itr%100==0):
+                self.acc=sess.run(accuracy,feed_dict={x:data,y:vali,kp1:1,kp2:1,kp3:1})
+                ed=time.clock()
+                print("file:%s  迭代次数:%5d   准确率:%7.3f%%  用时%7.3fs"%(fileName,itr,self.acc*100,ed-st))
+        
+        try:
+                
+            mat=[]    
+            for aa in [[x*0.01,y*0.01] for x in range(10,90) for y in range(10,90)]:
+                mat.append(aa)   
+            rmat=sess.run(fc[nFcl-1],feed_dict={x:mat,kp1:1,kp2:1,kp3:1})
+            rmat=np.transpose(rmat)
+    
+            X,Y=np.meshgrid([(float(x)*0.01-0.1)/0.8*tmaxa[0]+tmini[0] for x in range(10,90)],[((x*0.01)-0.1)/0.8*tmaxa[1]+tmini[1] for x in range(10,90)])
+            Z1=np.transpose(np.reshape(rmat[1],[len(X),len(Y)]))
+            Z0=np.transpose(np.reshape(rmat[0],[len(X),len(Y)]))
+            Z=Z1/(Z1+Z0)
+            plydata=[]
+            x = np.linspace(0,(float(90)*0.01-0.1)/0.8*tmaxa[0]+tmini[0],100)
+            for ity in range(len(X)):
+                for itx in range(len(X[0])):
+                    if(np.abs(Z[ity][itx]-0.5)<0.02):
+                        plydata.append([X[ity][itx],Y[ity][itx]])
+            plydata=np.transpose(plydata)
+            
+            import scipy.optimize as opt
+            self.fita,fitb=opt.curve_fit(self.FitFunc,plydata[0],plydata[1],[0.01,0.01,0.0,0.01,0,0.0])
+            
+            self.cof = np.polyfit(plydata[0],plydata[1],3)
+            func=np.poly1d(self.cof)
+        except:
+            print("Not 2d data!")
+        try:
+            plt.figure(1)
+            ax1=plt.subplot()
+            ax1.contourf(X,Y,Z,cmap=plt.get_cmap('seismic'))
+            for idx in range(len(tdata[0])):
+                ax1.scatter(data_t[idx][0],data_t[idx][1],marker=clr[idx][0])
+            ax1.plot(x,func(x),lw=1)
+            ax1.text(0,1,'$f(x)=%20.15f+%20.15fx+%20.15fx^2$'%(self.cof[2],self.cof[1],self.cof[0]))
+            ax1.text(0,0.8,'$f(x)=%fe^{%fx^2+%fx+%f}+%f+%fx$'%(self.fita[0],self.fita[1],self.fita[2],self.fita[3],self.fita[4],self.fita[5]))
+                     
+            ax1.plot(x,self.FitFunc(x,self.fita[0],self.fita[1],self.fita[2],self.fita[3],self.fita[4],self.fita[5]),lw=1)
+        except:
+            print("can not plot matrix")
+            
+        try:
+            plt.figure(2)
+            wv=[]
+            for itr in range(nFcl):
+                wv.append(sess.run(fc_w[itr].value()))
+            self.outwv=wv
+            ii=0
+            for lb in self.attri[:-1]:
+                plt.text(-0,ii,str(lb),horizontalalignment='right')
+                ii=ii+1
+            def PlotWeigh(www,layer):
+                www=np.abs(www)
+                mx=np.max(www)
+                for idy in range(len(www)):
+                    for idx in range(len(www[0])):
+                        plt.plot([layer-1,layer],[idx,idy],c='b',lw=www[idy][idx]*3/mx)
+                        plt.scatter([layer-1],[idx])
+                        plt.scatter([layer],[idy])
+                    plt.plot([layer,layer],[idx,idy],c='y',linestyle='--',lw=3)
+            for itr in range(nFcl):
+                #print(np.shape(wv[itr]))
+                PlotWeigh(np.transpose(wv[itr]),itr+1)
+        except:
+            print('can not plot weigh')
+
+
+        plt.show()    
+    
+    
+    
+if __name__ == '__main__':
+    
+    #节点数目
+    #[输入层数目，隐藏层节点数，.....，隐藏层节点数，输出层节点数]
+    layer=[10,8,4,2]
+    #读取文件名
+    fileName=r'index.xlsx'
+    valiFileName=r'index.xlsx'
+    #训练
+    aa=Classfication(fileName,valiFileName,layer=layer)
+    outFile=open('out.txt','w')
+    outFile.write('Function is:\n')
+    outFile.write('f(x)=%f+%f*x+%f*x^2(err:%f)\n'%(aa.cof[2],aa.cof[1],aa.cof[0],aa.acc))
+    outFile.write('f(x)=%fe^{%fx+%f}+%fe^{%fx+%f}\n'%(aa.fita[0],aa.fita[1],aa.fita[2],aa.fita[3],aa.fita[4],aa.fita[5]))
+    outFile.write('Weigh is:\n')
+    for ity in aa.outwv:
+        for it in ity:
+            outFile.write(str(it)+' ')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- WaveFilter/fl_with_class.py
+++ WaveFilter/fl_with_class.py
@@ -9,8 +9,9 @@ import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 import matplotlib.pyplot as plt
 import tensorflow as tf
-
-N=220
+import scipy.signal as sig
+from pysac import SacStreamIO
+N=256
 TN=2000
 class GenData():
     def __init__(self,shape=[1,1]):
@@ -27,7 +28,7 @@ class GenData():
         for itrd in range(self.shape[0]):
             sbn=1+np.random.randint(numSubWave)
             for itr in range(sbn):
-                f0=1+np.random.random(1)[0]*3
+                f0=1+np.random.random(1)[0]*0.5
                 Tc=1/(np.random.randint(5)+1)
                 per=100
                 t=np.linspace(0,1,per)
@@ -75,6 +76,7 @@ class GenDataSin():
         mx=np.max(self.data,axis=1)
         mx=np.reshape(mx,[-1,1])
         self.data=np.multiply(np.divide(self.data,mx),0.0)
+        #self.ndata=np.multiply(np.divide(self.data,mx),0.0)
         #return ariv
         
         return self.data
@@ -125,36 +127,38 @@ class FilterTrain():
 
 
     def __init__(self,func,raw_data):
+        c1s=[16,1,8]
+        c2s=[4,8,16]
+        c3s=[4,16,1]
+        
+        pool_type='MAX'
+        pool_window=[4,2,2]
+        pool_stride=[4,2,1]
         
         x = tf.placeholder(tf.float32,[50,N])
-        y = tf.placeholder(tf.float32,[50,N])
+        y = tf.placeholder(tf.float32,[50,int(N/pool_stride[0]/pool_stride[1]/pool_stride[2])])
         
-        c1s=[4,1,4]
-        c2s=[3,4,4]
-        c3s=[3,4,1]
-        
-        pool_type='AVG'
-        pool_window=[2,2,1]
+
         kp_prb=tf.placeholder('float')
         data1=tf.reshape(x,[50,N,1])
         self.c1_w=self.my_weigh(c1s)
         c1_b=self.my_weigh([c1s[2]])
         c1_f1=tf.nn.conv1d(data1,filters=self.c1_w,stride=1, padding='SAME')+c1_b
         c1_f2=tf.nn.tanh(c1_f1)
-        c1=tf.nn.pool(c1_f2,window_shape=[pool_window[0]],pooling_type=pool_type,strides=[1],padding='SAME')
+        c1=tf.nn.pool(c1_f2,window_shape=[pool_window[0]],pooling_type=pool_type,strides=[pool_stride[0]],padding='SAME')
         
         c2_w=self.my_weigh(c2s)
         c2_b=self.my_weigh([c2s[2]])
         c2_f1=tf.nn.conv1d(c1,filters=c2_w,stride=1, padding='SAME')+c2_b
         c2_f2=tf.nn.tanh(c2_f1)
         c2_f2=tf.nn.dropout(c2_f2,kp_prb)
-        c2=tf.nn.pool(c2_f2,window_shape=[pool_window[1]],pooling_type=pool_type,strides=[1],padding='SAME')
+        c2=tf.nn.pool(c2_f2,window_shape=[pool_window[1]],pooling_type=pool_type,strides=[pool_stride[1]],padding='SAME')
         
         c3_w=self.my_weigh(c3s)
         c3_b=self.my_weigh([c3s[2]])
         c3_f1=tf.nn.conv1d(c2,filters=c3_w,stride=1, padding='SAME')+c3_b
         c3_f2=tf.nn.tanh(c3_f1)
-        c3=tf.nn.pool(c3_f2,window_shape=[pool_window[2]],pooling_type=pool_type,strides=[1],padding='SAME')
+        c3=tf.nn.pool(c3_f2,window_shape=[pool_window[2]],pooling_type=pool_type,strides=[pool_stride[2]],padding='SAME')
         
         """
         c5_w=my_weigh(c5s)
@@ -193,7 +197,7 @@ class FilterTrain():
         
         
         
-        data2=tf.reshape(c3,[50,N])
+        data2=tf.reshape(c3,[50,int(N/pool_stride[0]/pool_stride[1]/pool_stride[2])])
         
         ce = tf.reduce_mean(tf.abs(data2-y))
         
@@ -204,21 +208,21 @@ class FilterTrain():
         
         func.__init__([50,N])
         data=func.GenWave()
-        data_noise=func.AddNoise(0.1)
+        data_noise=func.AddNoise(0.2)
         #print(sess.run(tf.shape(c2), feed_dict={x: data_noise, y: data,kp_prb:0.5}))
         
-        for itr in range(2000):
+        for itr in range(1000):
             if(itr%2!=7):
                 data=func.GenWave(3)
                 data_noise=func.AddNoise(0.0)
             else:
                 data=func.GenWave(0)
                 data_noise=func.AddNoise(0.1,tp='RD')
-            self.sess.run(train_step, feed_dict={x: data_noise, y: data,kp_prb:0.8})
+            self.sess.run(train_step, feed_dict={x: data_noise, y: sig.resample(data,int(N/pool_stride[0]/pool_stride[1]/pool_stride[2]),axis=1),kp_prb:0.8})
             #print(data[0])
             #plt.plot(data_noise[0])
             if(itr%100==0):
-                print("%d:%f"%(itr,self.sess.run(ce, feed_dict={x: data_noise, y: data,kp_prb:1.})))
+                print("%d:%f"%(itr,self.sess.run(ce, feed_dict={x: data_noise, y:sig.resample(data,int(N/pool_stride[0]/pool_stride[1]/pool_stride[2],),axis=1),kp_prb:1.})))
         #conv_layer=sess.run(c1_w, feed_dict={x: data_noise, y: data,kp_prb:0.1})
         
         
@@ -230,17 +234,17 @@ class FilterTrain():
         
         c1_f1_t=tf.nn.conv1d(data_t,filters=self.c1_w,stride=1, padding='SAME')+c1_b
         c1_f2_t=tf.nn.tanh(c1_f1_t)
-        c1_t=tf.nn.pool(c1_f2_t,window_shape=[pool_window[0]],pooling_type=pool_type,strides=[1],padding='SAME')
+        c1_t=tf.nn.pool(c1_f2_t,window_shape=[pool_window[0]],pooling_type=pool_type,strides=[pool_stride[0]],padding='SAME')
         
         c2_f1_t=tf.nn.conv1d(c1_t,filters=c2_w,stride=1, padding='SAME')+c2_b
         c2_f2_t=tf.nn.tanh(c2_f1_t)
         c2_f2_t=tf.nn.dropout(c2_f2_t,kp_t)
-        c2_t=tf.nn.pool(c2_f2_t,window_shape=[pool_window[1]],pooling_type=pool_type,strides=[1],padding='SAME')
+        c2_t=tf.nn.pool(c2_f2_t,window_shape=[pool_window[1]],pooling_type=pool_type,strides=[pool_stride[1]],padding='SAME')
         
         c3_f1_t=tf.nn.conv1d(c2_t,filters=c3_w,stride=1, padding='SAME')+c3_b
         c3_f2_t=tf.nn.tanh(c3_f1_t)
         c3_f2_t=tf.nn.dropout(c3_f2_t,kp_t)
-        c3_t=tf.nn.pool(c3_f2_t,window_shape=[pool_window[2]],pooling_type=pool_type,strides=[1],padding='SAME')
+        c3_t=tf.nn.pool(c3_f2_t,window_shape=[pool_window[2]],pooling_type=pool_type,strides=[pool_stride[2]],padding='SAME')
         
         
         """
@@ -269,59 +273,112 @@ class FilterTrain():
         c9_t=tf.nn.pool(c9_f2_t,window_shape=[1],pooling_type='AVG',strides=[1],padding='SAME')
         """
         kp_prb=tf.placeholder('float')
-        data2_t=tf.reshape(c3_t,sp)
+        data2_t=tf.reshape(c3_t,[sp[0],-1])
         
         #===========================================================================
         self.outdata=self.sess.run(data2_t, feed_dict={x_t: raw_data,kp_prb:1,kp_t:1})
-batch2 = GenData([50,TN])
-data=batch2.GenWave(100)
-data_noise=batch2.AddNoise(0.4)
+
+class GenDataErr():
+    def __init__(self,shape=[1,1]):
+        self.shape=shape
+        self.data=np.zeros(self.shape)
+        self.nzdata=np.zeros(self.shape)
+    def GenWave(self,numSubWave=30):
+        
+        #data=np.random.random(shape)
+        #data=np.subtract(data,0.5)
+        ariv=np.zeros([self.shape[0],1])
+        if(numSubWave==0):
+            return self.data
+        
+        for itrd in range(self.shape[0]):
+            sbn=1+np.random.randint(numSubWave)
+            for itr in range(sbn):
+                f0=1+np.random.random(1)[0]*0.5
+                Tc=1/(np.random.randint(5)+1)
+                per=100
+                t=np.linspace(0,1,per)
+                wave=(1+np.cos(2.*np.pi*(t-Tc/2.)/Tc))*np.cos(2.*np.pi*f0*(t-Tc/2.))/2.
+                blk=np.random.randint(30)   
+                vvae=np.array(wave[:])
+                vvae[blk:blk+10]=np.zeros([10])
+                #wave[blk:blk+30]=np.zeros([30])
+                nba=np.random.randint(self.shape[1]-per-1)
+                ariv[itrd][0]=1
+                self.data[itrd,nba:nba+per]=self.data[itrd,nba:nba+per]+wave
+                self.nzdata[itrd,nba:nba+per]=self.nzdata[itrd,nba:nba+per]+vvae
+        mx=np.max(self.data,axis=1)
+        mx=np.reshape(mx,[-1,1])
+        self.data=np.multiply(np.divide(self.data,mx),0.2)
+        #return ariv
+        self.nzdata=np.multiply(np.divide(self.nzdata,mx),0.2)
+        return self.data,self.nzdata
+    def AddNoise(self,bl=0.5,tp='FX'):
+        noise=np.random.random(self.shape)
+        noise=np.subtract(noise,0.5)
+        if(tp=='RD'):
+            bl=np.random.random(1)[0]*bl+0.0001
+        noise=np.multiply(noise,bl)
+        return np.add(self.data,noise)        
+        
+batch2 = GenDataErr([50,TN])
+data,nzdata=batch2.GenWave(5)
+data_noise=batch2.AddNoise(0.1)
 
 
 
-bb=GenDataLin()
-flt2=FilterTrain(bb,data_noise)
+bb=GenData()
+flt2=FilterTrain(bb,nzdata)
 outdata=flt2.outdata
 
+"""
+aa=SacStreamIO('st02_cut.z','rb')
 
+aa.DataDetrend()
+dt=aa.yVect
+dt1=0;np.random.random(np.shape(dt))*0.1
+dt=np.divide(np.reshape(dt,[-1,np.shape(dt)[0]]),np.max(dt)*5)
+dts=dt+dt1
+flt3=FilterTrain(bb,dts)
+dtflt=flt3.outdata
 
-
-cvl1=(np.transpose(np.reshape(flt2.sess.run(flt2.c1_w.value()),[4,4])))
+print(dtflt)
+cvl1=(np.transpose(np.reshape(flt2.sess.run(flt2.c1_w.value()),[4,8])))
 plt.figure(1)
 plt.subplot(511)
-plt.plot(data_noise[0],color='b',lw=1,alpha=0.2)
-plt.plot(outdata[0],color='cornflowerblue',lw=3)
-plt.plot(data[0],color='orange',lw=1,alpha=0.5)
-
-plt.subplot(512)
-plt.plot(data_noise[1],color='b',lw=1,alpha=0.2)
-plt.plot(outdata[1],color='cornflowerblue',lw=3)
+plt.plot(dts[0],color='b',lw=1,alpha=0.2)
+plt.plot(sig.resample(dtflt[0],np.shape(dt)[1]),color='cornflowerblue',lw=3)
+plt.plot(dt[0],color='orange',lw=1,alpha=0.5)
+"""
+plt.subplot(411)
+plt.plot(nzdata[1],color='b',lw=1,alpha=0.2)
+plt.plot(sig.resample(outdata[1],TN),color='cornflowerblue',lw=3)
 plt.plot(data[1],color='orange',lw=1,alpha=0.5)
 
-plt.subplot(513)
-plt.plot(data_noise[3],color='b',lw=1,alpha=0.2)
-plt.plot(MovAvg(data_noise[3],10),color='cornflowerblue',lw=3)
-plt.plot(data[3],color='orange',lw=1,alpha=0.5)
+plt.subplot(412)
+plt.plot(nzdata[2],color='b',lw=1,alpha=0.2)
+plt.plot(sig.resample(outdata[2],TN),color='cornflowerblue',lw=3)
+plt.plot(data[2],color='orange',lw=1,alpha=0.5)
 
-plt.subplot(514)
-plt.plot(data_noise[3],color='b',lw=1,alpha=0.2)
-plt.plot(outdata[3],color='cornflowerblue',lw=3)
+plt.subplot(413)
+plt.plot(nzdata[3],color='b',lw=1,alpha=0.2)
+plt.plot(sig.resample(outdata[3],TN),color='cornflowerblue',lw=3)
 plt.plot(data[3],color='orange',lw=1,alpha=0.5)
 
 import scipy.signal as signal
 b, a = signal.iirdesign([0.01, 0.15], [0.005, 0.17], 2, 40)
 out = signal.lfilter(b, a, data_noise[3])
 
-plt.subplot(515)
+plt.subplot(414)
 plt.plot(data_noise[3],color='b',lw=1,alpha=0.2)
-plt.plot(out,color='cornflowerblue',lw=3)
+plt.plot(sig.resample(out,TN),color='cornflowerblue',lw=3)
 plt.plot(data[3],color='orange',lw=1,alpha=0.5)
-
+"""
 plt.figure(2)
 plt.plot(np.transpose(cvl1))
-
+"""
 plt.show()
-
+"""
 for itry in range(3):
     outfile_no=open("no_data"+str(itry)+".txt","w")
     outfile_rw=open("rw_data"+str(itry)+".txt","w")
@@ -334,7 +391,7 @@ for itry in range(3):
         outfile_fl.write(str(outdata[itry][itrx])+"\n")
         #outz.WriteData(data_noise)
     #outz.CleanFile()
-        
+"""     
 
 
 
